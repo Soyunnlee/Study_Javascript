655ce436b25dc96f037489129d1ed6423c41ea20
TAB_SOYUN <soxxy95@naver.com>
2022-11-12 21:34:16 +0900

    function_11.12

--- functionTest1.js
+++ functionTest1.js
@@ -21,42 +21,80 @@ function meetAt (year,month,day){
 }
 console.log (meetAt(2022))
 
-//3. 해결못함.
-function findSmallestElement(arr) {
-	let result = arr[0];
-	if (arr === 0 ) {
-		return 0;
-	}
-  for (let i = 1;i < arr.length; i++){
-		if(result > arr[i]){
-			result = arr[i];
-		}
-	}
-	return result;
+
+
+
+//3. 제일 작은수를 반환하라
+function findSmallestElement (arr){
+    // 어레이가 비어있다.
+    if (arr.length === 0){
+        return 0
+    }
+    let result = arr[0] // 배열의 첫번째 값을 들고와서 비교할것이다.
+    // for문 : 100은 들고 있으므로 200부터 비교함. 
+    // 어디까지돌것인가? arr.length = 배열의 길이까지 돈다.
+    for(let i=1; i<arr.length; i++){
+        if(result > arr[i]){
+            //result 에 있는값이 arr[i] 번째에 있는 값보다 크다면
+            // arr[i] 에 있는값을 result 에 담아준다.
+            result = arr[i]
+        }
+    }  
+    return result
 }
 
-console.log(findSmallestElement([100,200,3,0,2,1]));
-
-// console.log(findSmallestElement([100,200,3,0,2,1]));
-// findSmallestElement([100,200,3,0,2,1]);
+// console.log(findSmallestElement([300,100,3,0,2,1]));
 
 
+//4. 몇개의 지폐와 동전이 필요한지
+// function money (a){
+//     let arr = [50000,10000,5000,1000,500,100]
 
+//     let bb = {
+//         50000 : 0 ,
+//         10000 : 0 , 
+//         5000 : 0 ,
+//         1000 : 0,
+//         500 : 0,
+//         100 : 0,
+//     }
 
-//4.
-// function moneyCount(money){
-//     // 10000 을 뺀 나머지 값
-//     let million = money % 10000
-//     // 10000 원의 개수
-//     let test1 = money - million
+//     for (let i=0; i <= arr.length ;i++){
+//         if( Math.floor(a/arr[i]) >= 1 ){ 
+//             // arr[i] 로 나눈 나머지값이 1 보다 크거나 같을경우
+//             result = Math.floor(a/arr[i])
+//             bb[arr[i]] = result
+//         } else {
+//             result = 0
+//         }
+//         a = a % arr[i];
+//         console.log(bb)
+//     }
 
-//     console.log(million,"dd")
-//     console.log(test1,"ddd")
-
-//     if( money % (5000 * 2) === 0){
-//         console.log(money)
-//         return "나머지값이 있음"
-//     } 
+//     console.log(bb[500])
 // }
 
-// moneyCount(12300)
+
+let unit = [50000,10000,5000,1000,500,100]
+// function changeCalculate(money) {
+//   for(let i=0;i<unit.length;i++){
+    
+//       let num = Math.floor(money / unit[i])
+//       console.log(unit[i]+"X"+num)
+//       money = money - (unit[i]*num)
+    
+//     }
+// }
+
+// changeCalculate(12600)
+
+
+function money(a){
+    for(let i = 0; i < unit.length; i++){
+        let num = Math.floor(a/unit[i])
+        console.log(unit[i] + "=" +num)
+        a = a % unit[i]
+    }
+}
+
+money(12600)
