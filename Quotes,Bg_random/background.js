ce9d01e4ce44dc4ba56a85d0ef804aa75208d2f7
Soyunnlee <soxxy95@naver.com>
2023-02-27 04:08:05 +0900

    random append/prepend

--- Quotes,Bg_random/background.js
+++ Quotes,Bg_random/background.js
@@ -4,10 +4,13 @@ const images = [
 
  const chosenImage = images[Math.floor(Math.random() * images.length)]
 
-const bgImage = document.createElement("img");
+const bgImage = document.createElement("img"); // <img>
 
-bgImage.src = `./img/${chosenImage}`
+bgImage.src = `./img/${chosenImage}` 
+// bgImage 의 scr 를 설정해준다.
+// <img src="./img/img1.jpg"/> 
 
-// "./img/img1.jpg"
+document.body.appendChild(bgImage)
+// appendChild() 는 body 에 html을 추가한다.
 
 console.log(bgImage)
