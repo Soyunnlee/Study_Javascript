74169ac68d388efe64c8fa32c83b0bc17be10239
Soyunnlee <soxxy95@naver.com>
2023-02-28 14:03:58 +0900

    target remove

--- Todo/todo.js
+++ Todo/todo.js
@@ -2,22 +2,34 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.getElementById("todo-list");
 
+// x 버튼을 눌렀을떼
+function deleteTodo(event){
+    const li = event.target.parentElement
+    li.remove();
+}
+
 function paintTodo(newTodo){
-    console.log("iwill paint newTodo",newTodo)
     const li = document.createElement("li")
     const span = document.createElement("span")
-    
+    const button = document.createElement("button")
+    button.innerText = "X"
+    // x 버튼을 눌렀을떼
+    button.addEventListener("click" , deleteTodo)
+
+
     // li 안에 span 을 추가한다.
-    li.appendChild(span);    
+    li.appendChild(span);
+    li.appendChild(button);
     span.innerText = newTodo;
 
-    toDoList.appendChild(li);
+    toDoList.appendChild(li);  
 }
+
 
 function handleTodoSubmit(event){
     event.preventDefault();
     const newTodo = toDoInput.value;
-    localStorage.setItem(newTodo,newTodo);
+    localStorage.setItem("toDoKey",newTodo);
     // newTodo 와 로컬스토리지에 input.value 값을 복사해주면 input 값을 비워준다.
     toDoInput.value = "";
 
