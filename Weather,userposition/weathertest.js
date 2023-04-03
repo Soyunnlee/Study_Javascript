e1ce954098ed7a5d44eea5f7159175752e113993
Soyunnlee <soxxy95@naver.com>
2023-04-04 00:14:01 +0900

    fetch Api Repetition

---
+++ Weather,userposition/weathertest.js
@@ -0,0 +1,25 @@
+
+const API_KEY = '160d89b34edbb491a986e42383086c43'
+
+const printweather = document.getElementById("weather")
+console.log(printweather)
+
+
+  function fetchweather(){
+    const lat = '37.4989073'
+    const lon = '126.9214082'
+
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`)
+        .then((response) => response.json())
+        .then((data) => {
+            const myposition = data.name
+            const weather = data.weather[0].main
+            const span = document.createElement("span");
+
+            printweather.appendChild(span);
+            span.innerText =`내 위치는 ${myposition} 이고, 날씨는 ${weather} 이다.`
+     
+        }); 
+}
+
+fetchweather()
